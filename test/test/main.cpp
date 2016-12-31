e0b16c003b77b38d930fc02df2f4b913d8a6d2ae
ainiroad <l3_zzz@163.com>
2016-12-31 22:11:20 +0800

    add distributed

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Ainiroad on 2016/12/31.
+//  Copyright © 2016年 Ainiroad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
